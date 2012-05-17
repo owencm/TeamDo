29b5c1d09cd15ed943a86e75a686482d455a06cc
Owen Campbell-Moore <owencmoore@gmail.com>
2012-05-17 23:33:45 +0100

    Removed password from user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120513173630) do
+ActiveRecord::Schema.define(:version => 20120517223241) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(:version => 20120513173630) do
     t.string   "last"
     t.string   "email"
     t.string   "username"
-    t.string   "password"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
