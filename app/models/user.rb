a6d9ad5b1b88c54b783d555d5697f36e531852d4
ide user ide_owencm <owencmoore@gmail.com>
2012-05-17 00:33:23 +0000

    Bug fix of user model and added comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   
   has_many :task_matches, :foreign_key => "user_id", :class_name => "TaskDoer"
   
-  has_many :to_me_tasks, :through => :task_matches, :source => "Task"
+  has_many :to_me_tasks, :through => :task_matches, :source => "task" #source indicates what to look for in the join table
   
   attr_accessible :created, :email, :first, :last, :password, :username
   
