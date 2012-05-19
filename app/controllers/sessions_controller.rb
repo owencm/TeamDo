f1b47c1f6fcbf634d839ac49a67c9bafd831e946
Owen Campbell-Moore <owencmoore@gmail.com>
2012-05-19 11:41:28 +0100

    Added haml file

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def create
+    @user = User.find(params[:id])
+    session[:user] = @user
+
+    respond_to do |format|
+      format.html { redirect_to @user, notice: 'Welcome, '+@user.first+"." }
+      format.json { head :no_content }
+    end
+  end
+  
+  def new
+    @users = User.all
+    respond_to do |format|
+      format.html # new.erb.html
+      format.json { render json: @users }
+    end
+  end
+  
+end
