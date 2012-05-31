052e345b2ae29adf3a96d38f208a1d7b5a2f13cf
Owen Campbell-Moore <owencmoore@gmail.com>
2012-05-31 19:59:54 +0100

    Refactored groups to not have logic in view

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,20 @@ class User < ActiveRecord::Base
   
   has_many :groups, :through => :group_users, :source => "group"
   
+  def outstanding_tasks 
+    self.to_me_tasks.find_all_by_completed_at(nil)
+  end
+  
+  def past_tasks
+      self.to_me_tasks.where("completed_at IS NOT NULL")
+  end
+  
+  def has_outstanding_tasks
+    outstanding_tasks.count > 0
+  end
+  
+  def has_past_tasks
+    past_tasks.count > 0
+  end
+  
 end
