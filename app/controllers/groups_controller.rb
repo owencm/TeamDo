5f001bfaac6f74b7c930e38b96f91b4fa98748a8
Owen Campbell-Moore <owencmoore@gmail.com>
2012-07-08 22:00:18 -0700

    Notifications now use a jquery plugin

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,9 +7,8 @@ class GroupsController < ApplicationController
   # GET /groups.json
   def index
     @groups = Group.all
-
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.json { render json: @groups }
     end
   end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -70,7 +70,7 @@ class TasksController < ApplicationController
       @task = Task.new(params[:task])
       respond_to do |format|
         if @task.save
-          format.html { redirect_to @task, notice: 'Task was successfully created.' }
+          format.html { redirect_to @task.group, notice: 'The task was successfully assigned.' }
           format.json { render json: @task, status: :created, location: @task }
         else
           format.html { render action: "new" }
